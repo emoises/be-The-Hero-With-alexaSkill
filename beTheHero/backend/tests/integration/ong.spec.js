1fce5346931a88f3a4db87bd793adda74e612e87
emoises <51842925+emoises@users.noreply.github.com>
2020-05-11 15:42:45 -0300

    implementação do backend

---
+++ beTheHero/backend/tests/integration/ong.spec.js
@@ -0,0 +1,25 @@
+const request = require('supertest');
+const app = require('../../src/app');
+const connection = require('../../src/database/connection')
+
+describe('ONG', () => {
+    beforeEach( async ()=> {
+        await connection.migrate.latest();
+    });
+    afterAll( async () => {
+        await connection.destroy();
+    });
+    it('should be able to create a new ONG.', async () => {
+         const response = await request(app)
+         .post('/ongs')
+         .send({
+            name: "APAD2",
+            email: "email@email.com",
+            whatsapp: "21909092121",
+            city: "Rio de janeiro",
+            uf: "RJ",
+        });
+        expect(response.body).toHaveProperty('id');
+        expect(response.body.id).toHaveLength(8);
+    })
+});
